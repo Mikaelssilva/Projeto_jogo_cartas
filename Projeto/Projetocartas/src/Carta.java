c0c4fd4166bc879453f56d9bf97df746ef7f2211
Arthur Pontes Ferreira <apf3@poli.br>
2024-10-08 21:11:18 -0300

    Add files via upload

---
+++ Projeto/Projetocartas/src/Carta.java
@@ -0,0 +1,30 @@
+public class Carta {
+  private String nome;
+  private int custoDeMana; 
+
+  public Carta(String nome, int custodemana) {
+    setNome(nome);
+    setCustoDeMana(custodemana);
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getCustoDeMana() {
+    return custoDeMana;
+  }
+
+  public void setCustoDeMana(int custoDeMana) {
+    if (custoDeMana <= 10) {
+      this.custoDeMana = custoDeMana;
+    } else {
+      System.out.println("Custo de mana excede o limite de 10");
+      System.exit(1);
+    }
+  }
+}
---
+++ Projeto/Projetocartas/src/Deck.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+	private List<Carta> deck = new ArrayList<Carta>();
+	public void addCard(Carta carta) {
+		deck.add(carta);
+	}
+	public int size(){
+		return this.deck.size();
+	}
+	public Carta getCarta(int i){
+		return this.deck.get(i);
+	}
+}
+
